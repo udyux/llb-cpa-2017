4164410e958d1a835add60ef24a569388bf07fae
Nicolas Udy <dev@udy.io>
2017-12-20 20:39:08 -0500

    Final touchups

--- scripts/modules/card-flip.js
+++ scripts/modules/card-flip.js
@@ -1,10 +1,19 @@
 import dom from './dom-helpers'
 
-const getFlipHandler = (node) => () => node.classList.add('card--flip')
-const getUnflipHandler = (node) => () => node.classList.remove('card--flip')
+const getFlipHandler = (node) =>
+  () => node.classList.add('card--flip')
+
+const getUnflipHandler = (node) =>
+  () => node.classList.remove('card--flip')
+
+const getShortestSide = ([front, back]) =>
+  (front.clientHeight < back.clientHeight) ? front : back
 
 dom.findAll('[data-card]')
   .forEach(node => {
+    getShortestSide(node.children)
+      .style.position = 'absolute'
+
     dom.findAll('[data-card-flip]', node)
       .forEach(flipNode => {
         flipNode.addEventListener('click', getFlipHandler(node))
