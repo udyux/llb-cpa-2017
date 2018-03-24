1b8c570a57ca4df341f441f606b644b115a5eea1
Nicolas Udy <dev@udy.io>
2018-03-23 21:13:16 -0400

    Add resize handler

--- scripts/modules/card-flip.js
+++ scripts/modules/card-flip.js
@@ -1,26 +1,50 @@
 import dom from './dom-helpers'
 
+const nodes = dom.findAll('[data-card]')
+
 const getFlipHandler = (node) =>
   () => node.classList.add('card--flip')
 
 const getUnflipHandler = (node) =>
   () => node.classList.remove('card--flip')
 
-const getShortestSide = ([front, back]) =>
-  (front.clientHeight < back.clientHeight) ? front : back
+const sortByHeight = ([front, back]) =>
+  (front.clientHeight < back.clientHeight) ? [front, back] : [back, front]
 
-dom.findAll('[data-card]')
-  .forEach(node => {
-    getShortestSide(node.children)
-      .style.position = 'absolute'
+const getResizeBuffer = () => {
+  let buffer
 
-    dom.findAll('[data-card-flip]', node)
-      .forEach(flipNode => {
-        flipNode.addEventListener('click', getFlipHandler(node))
+  const updateNodes = () => {
+    nodes.forEach(node => {
+      sortByHeight(node.children).forEach((el, i) => {
+        if (!i) el.style.position = 'absolute'
+        else el.style.position = 'relative'
       })
+    })
 
-    dom.findAll('[data-card-unflip]', node)
-      .forEach(unflipNode => {
-        unflipNode.addEventListener('click', getUnflipHandler(node))
-      })
-  })
+    buffer = false
+  }
+
+  updateNodes()
+
+  return () => {
+    if (!buffer) window.requestAnimationFrame(updateNodes)
+    buffer = true
+  }
+}
+
+nodes.forEach(node => {
+  dom.findAll('[data-card-flip]', node)
+    .forEach(flipNode => {
+      flipNode.addEventListener('click', getFlipHandler(node))
+    })
+
+  dom.findAll('[data-card-unflip]', node)
+    .forEach(unflipNode => {
+      unflipNode.addEventListener('click', getUnflipHandler(node))
+    })
+})
+
+setTimeout(() => {
+  window.addEventListener('resize', getResizeBuffer())
+}, 500)
